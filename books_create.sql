0ad32987d531b59e98a502779513c987a4356e5e
Pranav Balakrishnan <58393352+pranavbalakrishnan4100@users.noreply.github.com>
2021-09-01 00:58:21 +0530

    Mock data files

---
+++ books_create.sql
@@ -0,0 +1,60 @@
+create table books (
+	book_id INT,
+	isbn VARCHAR(50),
+	title VARCHAR(50),
+	author VARCHAR(50),
+	publisher VARCHAR(50),
+	category VARCHAR(50),
+	status VARCHAR(50),
+	issued_to VARCHAR(50)
+);
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (1, '470105393-7', 'Elvira, Mistress of the Dark', 'Angele Harrie', 'Non-binary', 'Comedy|Horror', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (2, '572745663-0', 'Alex in Wonderland', 'Laure Dunkerton', 'Genderqueer', 'Comedy|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (3, '414498596-2', 'Part of the Weekend Never Dies', 'Harwell Marcum', 'Male', 'Documentary', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (4, '572052588-2', 'Hell of a Day, A (Reines d''un jour)', 'Eldredge Sandiford', 'Bigender', 'Comedy|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (5, '994070638-3', 'Swedish Auto', 'Ketti Di Iorio', 'Agender', 'Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (6, '758915847-X', 'Telling You', 'Stanley Agett', 'Non-binary', 'Comedy|Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (7, '845475265-X', 'Thin Man, The', 'Susanne Schapiro', 'Polygender', 'Comedy|Crime', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (8, '381194476-2', 'Public Eye, The (Follow Me!)', 'Persis Probat', 'Non-binary', 'Comedy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (9, '650815977-1', 'Frontier of the Dawn (La frontière de l''aube)', 'Morton Gammill', 'Male', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (10, '456664345-X', 'Dummy', 'Esra Thunnercliff', 'Polygender', 'Comedy|Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (11, '190521363-8', 'Toy Story Toons: Partysaurus Rex', 'Tobi McSpirron', 'Male', 'Animation|Children|Comedy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (12, '100998815-8', 'Hounddog', 'Laina Paik', 'Polygender', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (13, '285754145-7', 'East Palace West Palace (Dong gong xi gong)', 'Tommie Gasnell', 'Bigender', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (14, '437336158-4', 'Alien Abduction', 'Dorotea Farquhar', 'Genderqueer', 'Horror|Mystery|Sci-Fi', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (15, '006254960-X', 'Gold Diggers of 1933', 'Charlton Eamer', 'Female', 'Musical', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (16, '376126203-5', 'Julie & Julia', 'Maible Gabbott', 'Genderfluid', 'Comedy|Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (17, '766875810-0', 'Miracle Worker, The', 'Ewen Flecknell', 'Female', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (18, '271366479-9', 'Johnson Family Vacation', 'Vivyan McGriffin', 'Genderqueer', 'Comedy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (19, '900386100-5', 'True Blue ', 'Coral Hillock', 'Bigender', 'Crime|Drama|Thriller', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (20, '411342675-9', 'Sacrificio: Who Betrayed Che Guevara', 'Saundra Hawick', 'Bigender', 'Documentary', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (21, '041890600-9', 'Tears of Steel', 'Leonora Rushbury', 'Agender', 'Sci-Fi', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (22, '729208390-0', 'Gregory Go Boom', 'Amalee Roeby', 'Agender', 'Comedy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (23, '261297813-5', 'Happy-Go-Lucky', 'Sigismund Scheu', 'Male', 'Comedy|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (24, '764253193-1', 'Nowhere in Africa (Nirgendwo in Afrika)', 'Gavin Fenty', 'Agender', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (25, '226978280-1', 'Iron Rose, The (Rose de fer, La)', 'Bryon Brokenshire', 'Non-binary', 'Horror', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (26, '044619647-9', 'Sharktopus vs. Pteracuda', 'Boniface McNickle', 'Genderfluid', 'Sci-Fi', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (27, '918113265-4', 'Lincoln', 'Sid Anstie', 'Genderfluid', 'Drama|War', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (28, '581915782-6', 'Cimarron Kid, The', 'Salomo Durrington', 'Genderqueer', 'Western', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (29, '485857473-3', 'Alien Abduction', 'Kelsy Keedwell', 'Polygender', 'Horror|Mystery|Sci-Fi', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (30, '384160253-3', 'Prisoner of Zenda, The', 'Jeni Le Provest', 'Genderfluid', 'Adventure', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (31, '053166072-9', 'I Accuse!', 'Annamaria Stoeck', 'Non-binary', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (32, '301602821-2', 'Night at the Roxbury, A', 'Karalee Bracey', 'Genderfluid', 'Comedy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (33, '684845814-7', 'Mosquito Net, The (La mosquitera)', 'Ricardo Kleinpeltz', 'Bigender', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (34, '414986255-9', 'Dark Side of the Sun, The', 'Angelika Hambrick', 'Agender', 'Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (35, '019602050-6', 'L.627', 'Min Abramovitz', 'Genderqueer', 'Crime|Drama|Thriller', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (36, '242312556-9', 'Detour', 'Oswell Moret', 'Agender', 'Crime|Film-Noir', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (37, '865769057-8', 'Hateship Loveship', 'Delilah Branigan', 'Genderqueer', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (38, '072505456-5', 'Alibi, The (Lies and Alibis)', 'Daron Braidwood', 'Non-binary', 'Comedy|Drama|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (39, '037333650-0', 'The Boys', 'Annalise Moncrefe', 'Agender', 'Crime|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (40, '439368893-7', 'Brides of Dracula', 'Pierson McKelloch', 'Genderfluid', 'Horror', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (41, '918980963-7', 'Untouchables, The', 'Westbrooke Eastup', 'Genderfluid', 'Action|Crime|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (42, '085894417-0', 'Make Like a Thief (Juokse kuin varas)', 'Debor Wookey', 'Female', 'Comedy|Crime|Thriller', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (43, '882423838-6', 'Bernie', 'Cyril Eastcott', 'Male', 'Comedy|Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (44, '058348384-4', 'Quigley Down Under', 'Ursulina Buffery', 'Genderfluid', 'Adventure|Drama|Western', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (45, '086208405-9', 'Or (a.k.a. My Treasure)', 'Page Sorsbie', 'Female', 'Drama', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (46, '342665883-6', 'Son of the White Mare', 'Yank Titterington', 'Genderfluid', 'Adventure|Animation|Fantasy', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (47, '230594743-7', 'Rendition', 'Brynne Branno', 'Female', 'Drama|Thriller', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (48, '565204337-7', 'Beauty and the Beast', 'Brannon Losbie', 'Polygender', 'Children|Fantasy|Horror|Romance', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (49, '303017567-7', 'Monkey Shines', 'Hayyim Cisco', 'Polygender', 'Horror|Sci-Fi', 'available', '');
+insert into books (book_id, isbn, title, author, publisher, category, status, issued_to) values (50, '178548240-8', 'Tycoon (Oligarkh)', 'Jonah Danihelka', 'Non-binary', 'Crime|Drama', 'available', '');
---
+++ members_create.sql
@@ -0,0 +1,56 @@
+create table members (
+	mem_id INT,
+	name VARCHAR(50),
+	phone VARCHAR(50),
+	book_issued VARCHAR(50)
+);
+insert into members (mem_id, name, phone, book_issued) values (1, 'Michail Dwane', '8389005608', '');
+insert into members (mem_id, name, phone, book_issued) values (2, 'Sheff Rosetti', '1689201883', '');
+insert into members (mem_id, name, phone, book_issued) values (3, 'Chase Belle', '7132493088', '');
+insert into members (mem_id, name, phone, book_issued) values (4, 'Brita Altree', '9155376036', '');
+insert into members (mem_id, name, phone, book_issued) values (5, 'Nehemiah Bresman', '1366578581', '');
+insert into members (mem_id, name, phone, book_issued) values (6, 'Killy Jenoure', '2661391674', '');
+insert into members (mem_id, name, phone, book_issued) values (7, 'Janet Tammadge', '4418164207', '');
+insert into members (mem_id, name, phone, book_issued) values (8, 'Sanson Coase', '2015058038', '');
+insert into members (mem_id, name, phone, book_issued) values (9, 'Merci Boud', '9988727381', '');
+insert into members (mem_id, name, phone, book_issued) values (10, 'Walther Wickey', '1702947173', '');
+insert into members (mem_id, name, phone, book_issued) values (11, 'Kordula Urquhart', '4745629769', '');
+insert into members (mem_id, name, phone, book_issued) values (12, 'Dori Chesney', '1004232964', '');
+insert into members (mem_id, name, phone, book_issued) values (13, 'Kaine Lillgard', '2816512026', '');
+insert into members (mem_id, name, phone, book_issued) values (14, 'Bank Pengelley', '8832540300', '');
+insert into members (mem_id, name, phone, book_issued) values (15, 'Morgen Arpino', '2757973772', '');
+insert into members (mem_id, name, phone, book_issued) values (16, 'Kathy Domeney', '5514555914', '');
+insert into members (mem_id, name, phone, book_issued) values (17, 'Felecia Coate', '1343630001', '');
+insert into members (mem_id, name, phone, book_issued) values (18, 'Juliann Frankiewicz', '6983352613', '');
+insert into members (mem_id, name, phone, book_issued) values (19, 'Davis Donwell', '6857123624', '');
+insert into members (mem_id, name, phone, book_issued) values (20, 'Hewet Ventris', '8387736056', '');
+insert into members (mem_id, name, phone, book_issued) values (21, 'Locke Tewnion', '2145189594', '');
+insert into members (mem_id, name, phone, book_issued) values (22, 'Mychal Hedylstone', '5475811086', '');
+insert into members (mem_id, name, phone, book_issued) values (23, 'Darryl Richardt', '1088859916', '');
+insert into members (mem_id, name, phone, book_issued) values (24, 'Guthrie Wiltsher', '8485587550', '');
+insert into members (mem_id, name, phone, book_issued) values (25, 'Coriss Roakes', '6254606513', '');
+insert into members (mem_id, name, phone, book_issued) values (26, 'Baily Showalter', '4687137868', '');
+insert into members (mem_id, name, phone, book_issued) values (27, 'Garfield Wanjek', '5542828823', '');
+insert into members (mem_id, name, phone, book_issued) values (28, 'Susan Rooze', '5556281412', '');
+insert into members (mem_id, name, phone, book_issued) values (29, 'Claude Cockton', '5116979669', '');
+insert into members (mem_id, name, phone, book_issued) values (30, 'Jacinda Hawes', '7476119244', '');
+insert into members (mem_id, name, phone, book_issued) values (31, 'Abrahan Arons', '1856211583', '');
+insert into members (mem_id, name, phone, book_issued) values (32, 'Myrvyn Eller', '4344114449', '');
+insert into members (mem_id, name, phone, book_issued) values (33, 'Carmela Labadini', '6758130996', '');
+insert into members (mem_id, name, phone, book_issued) values (34, 'Freddy Batchan', '6508140697', '');
+insert into members (mem_id, name, phone, book_issued) values (35, 'Maximo Eivers', '1134149641', '');
+insert into members (mem_id, name, phone, book_issued) values (36, 'Ramonda Chatain', '5567273417', '');
+insert into members (mem_id, name, phone, book_issued) values (37, 'Kenyon Dunsire', '6972085646', '');
+insert into members (mem_id, name, phone, book_issued) values (38, 'Karolina Liver', '8852650449', '');
+insert into members (mem_id, name, phone, book_issued) values (39, 'Gray Heathfield', '5916890044', '');
+insert into members (mem_id, name, phone, book_issued) values (40, 'Aurel Webley', '6939374863', '');
+insert into members (mem_id, name, phone, book_issued) values (41, 'Klarrisa Chopin', '1517683158', '');
+insert into members (mem_id, name, phone, book_issued) values (42, 'Willy Killgus', '1416417052', '');
+insert into members (mem_id, name, phone, book_issued) values (43, 'Jaymee Stoeck', '7691503378', '');
+insert into members (mem_id, name, phone, book_issued) values (44, 'Amil Wilfinger', '8407281680', '');
+insert into members (mem_id, name, phone, book_issued) values (45, 'Consolata Scantlebury', '7405215351', '');
+insert into members (mem_id, name, phone, book_issued) values (46, 'Maryanna Authers', '6963556735', '');
+insert into members (mem_id, name, phone, book_issued) values (47, 'Rutherford Maffin', '9178195253', '');
+insert into members (mem_id, name, phone, book_issued) values (48, 'Dolf Pittway', '2749761282', '');
+insert into members (mem_id, name, phone, book_issued) values (49, 'Stacy Versey', '9152449705', '');
+insert into members (mem_id, name, phone, book_issued) values (50, 'Hedwig Longshaw', '7531671022', '');
